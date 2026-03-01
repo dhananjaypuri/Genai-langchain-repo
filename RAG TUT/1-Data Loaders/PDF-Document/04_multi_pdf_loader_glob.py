3b7d567765c4fe3b8701d3f69ea00266456efd70
dhananjaypuri <50763591+dhananjaypuri@users.noreply.github.com>
2026-03-01 16:33:22 +0530

    K

--- RAG TUT/1-Data Loaders/PDF-Document/04_multi_pdf_loader_glob.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-
-loader = DirectoryLoader(path='pdfs', glob='*.pdf', loader_cls=PyPDFLoader);
-
-docs = loader.lazy_load();
-
-for doc in docs:
-    print(doc.page_content);
-    print("\n\n");
--- RAG TUT/1-Data Loaders/Web-Based-Doc-Loader/01.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-loader = WebBaseLoader("https://docs.langchain.com/oss/python/integrations/document_loaders#pdfs");
-
-docs = loader.load();
-
-print(docs[0].page_content);
